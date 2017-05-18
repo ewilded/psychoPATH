4bb6b3b996d2bd0fd2423c67c5fdf3f11c1e5d14
Julian Horoszkiewicz <ewilded@gmail.com>
2017-05-18 14:11:21 +0100

    Add files via upload

---
+++ Java/src/PsychoPATH.java
@@ -0,0 +1,11 @@
+package uk.co.pentest.psychoPATH;
+
+import burp.IBurpExtenderCallbacks;
+
+/**
+ * Used to store global variables.
+ */
+public class PsychoPATH {
+    public static IBurpExtenderCallbacks callbacks;
+    public static PsychoTab PsychoTab;
+}
