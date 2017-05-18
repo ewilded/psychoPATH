103457078261d9c74346fa6ceb2f5e0362eb8555
Julian Horoszkiewicz <ewilded@gmail.com>
2017-05-18 14:09:31 +0100

    Add files via upload

---
+++ Java/BurpExtender.java
@@ -0,0 +1,38 @@
+package burp;
+
+
+import uk.co.pentest.psychoPATH.PsychoPATH;
+import uk.co.pentest.psychoPATH.PsychoTab;
+import uk.co.pentest.psychoPATH.PayloadFactory;
+
+/**
+ * The main entry class that Burp calls to load/unload the extension.
+ */
+public class BurpExtender implements IBurpExtender, IExtensionStateListener {
+
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
+        PsychoPATH.callbacks = callbacks;
+        callbacks.setExtensionName("psychoPATH Extension");
+        PsychoPATH.PsychoTab = new PsychoTab();
+        callbacks.addSuiteTab(PsychoPATH.PsychoTab);
+        
+        callbacks.registerExtensionStateListener(this);
+        
+        // now we register two intruder extension factories; one for the traversed filename, one for the payload marker
+        callbacks.registerIntruderPayloadGeneratorFactory(new PayloadFactory(PsychoPATH.PsychoTab, "path"));
+        callbacks.registerIntruderPayloadGeneratorFactory(new PayloadFactory(PsychoPATH.PsychoTab, "mark"));
+        callbacks.registerIntruderPayloadGeneratorFactory(new PayloadFactory(PsychoPATH.PsychoTab, "check"));
+        callbacks.registerContextMenuFactory(new Menu(callbacks));
+    }
+
+    @Override
+    public void extensionUnloaded() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public static IBurpExtenderCallbacks getBurpCallbacks() {
+        return PsychoPATH.callbacks;
+    }
+    
+
+}
