c114e1608969417799657195204d322ee080b557
Julian Horoszkiewicz <ewilded@gmail.com>
2017-05-21 12:58:07 +0200

    Update three.php

--- test_cases/LFI/three.php
+++ test_cases/LFI/three.php
@@ -3,9 +3,8 @@
 if(isset($_GET['file']))
 {
 	$file=str_replace('../','',$_GET['file']);
-	$file=str_replace('./','',$_GET['file']);
+	$file=str_replace('./','',$file);
 	echo @file_get_contents('./'.$file);
 }
 #removing ../ then ./
 ?>
-
