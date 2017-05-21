4d807ed1d5c69ba97990e7f7cf9906119d32e788
Julian Horoszkiewicz <ewilded@gmail.com>
2017-05-21 11:21:50 +0100

    Add files via upload
    Fun LFI samples easily discoverable with psychoPATH

---
+++ test_cases/LFI/three.php
@@ -0,0 +1,11 @@
+<?php
+
+if(isset($_GET['file']))
+{
+	$file=str_replace('../','',$_GET['file']);
+	$file=str_replace('./','',$_GET['file']);
+	echo @file_get_contents('./'.$file);
+}
+#removing ../ then ./
+?>
+
---
+++ test_cases/LFI/two.php
@@ -0,0 +1,10 @@
+<?php
+
+if(isset($_GET['file']))
+{
+	$file=str_replace('./','',$_GET['file']);
+	echo @file_get_contents('./'.$file);
+}
+#removing only ./
+?>
+
